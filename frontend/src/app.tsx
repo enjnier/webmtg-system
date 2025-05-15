8f3a74bb675af5f2b7a21304030c57a8e621beb4
enjnier <71798649+enjnier@users.noreply.github.com>
2025-05-16 00:13:58 +0900

    fix: 既存tsxファイルのフォーマット

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/shared/components/ui/button";
+import { Button } from '@/shared/components/ui/button'
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
